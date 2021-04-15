58fc434362bff60db5f5fd63e16bdca1d92258eb
Victor Truong <victor@victortruong.com>
2021-04-15 10:31:36 -0700

    Add GetEvents method

--- event.go
+++ event.go
@@ -1,5 +1,7 @@
 package clubhouse
 
+import "net/http"
+
 type Event struct {
 	Channel      *string     `json:"channel"`
 	Club         *Club       `json:"club"`
@@ -19,4 +21,25 @@ type EventHost struct {
 	Twitter *string `json:"twitter"`
 }
 
-// TODO
+type GetEventsParams struct {
+	IsFiltered bool `url:"is_filtered"`
+
+	Page     *int `json:"-" url:"page,omitempty"`
+	PageSize *int `json:"-" url:"page_size,omitempty"`
+}
+
+type GetEventsResponse struct {
+	PageResponse
+	Events []struct {
+		Event
+		ClubIsFollower int `json:"club_is_follower"`
+		ClubIsMember   int `json:"club_is_member"`
+	} `json:"events"`
+}
+
+func (c *Client) GetEvents(params *GetEventsParams) (*GetEventsResponse, *http.Response, error) {
+	apiRes := new(GetEventsResponse)
+	apiError := new(APIError)
+	res, err := c.sling.New().Get("get_events").QueryStruct(params).Receive(apiRes, apiError)
+	return apiRes, res, relevantError(err, *apiError)
+}

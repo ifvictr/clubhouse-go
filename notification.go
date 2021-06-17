5c1de5fc0d08b7a2f9bba2966694ecc1049820dd
Victor Truong <victor@victortruong.com>
2021-06-16 18:19:48 -0700

    Add Notification type and GetNotifications method

--- notification.go
+++ notification.go
@@ -1,3 +1,33 @@
 package clubhouse
 
-// TODO
+import "net/http"
+
+type Notification struct {
+	Channel        *string     `json:"channel,omitempty"`
+	EventID        *int        `json:"event_id,omitempty"`
+	IsUnread       bool        `json:"is_unread"`
+	Message        string      `json:"message"`
+	NotificationID int         `json:"notification_id"`
+	TimeCreated    string      `json:"time_created"`
+	Type           int         `json:"type"`
+	UserProfile    UserProfile `json:"user_profile"`
+}
+
+type GetNotificationsParams struct {
+	Page     *int `json:"-" url:"page,omitempty"`
+	PageSize *int `json:"-" url:"page_size,omitempty"`
+}
+
+type GetNotificationsResponse struct {
+	Response
+	Count         int            `json:"count"`
+	Disabled      bool           `json:"disabled"`
+	Notifications []Notification `json:"notifications"`
+}
+
+func (c *Client) GetNotifications(params *GetNotificationsParams) (*GetNotificationsResponse, *http.Response, error) {
+	apiRes := new(GetNotificationsResponse)
+	apiError := new(APIError)
+	res, err := c.sling.New().Get("get_notifications").QueryStruct(params).Receive(apiRes, apiError)
+	return apiRes, res, relevantError(err, *apiError)
+}

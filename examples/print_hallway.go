c94093437d005c9a317985b1a15a9dc5d4800060
Victor Truong <victor@victortruong.com>
2021-06-18 18:55:54 -0700

    Add print_hallway example

---
+++ examples/print_hallway.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+
+	"github.com/ifvictr/clubhouse-go"
+)
+
+func main() {
+	token := flag.String("token", "", "Clubhouse account auth token")
+	flag.Parse()
+
+	// Clubhouse client
+	client := clubhouse.NewFromToken(*token)
+
+	res, _, err := client.GetChannels()
+	if err != nil {
+		fmt.Printf("Could not get channels: %s\n", err)
+		return
+	}
+
+	// Print out channels in hallway
+	for i, channel := range res.Channels {
+		var topic string
+		if channel.Topic != nil {
+			topic = *channel.Topic
+		}
+
+		fmt.Printf("%2d | %s | %s | %s\n", i+1, getVisibilityEmoji(channel.IsPrivate, channel.IsSocialMode), channel.Channel, topic)
+	}
+}
+
+func getVisibilityEmoji(private bool, social bool) string {
+	switch true {
+	case private:
+		return "🔒"
+	case social:
+		return "👥"
+	default:
+		return "🌎"
+	}
+}
